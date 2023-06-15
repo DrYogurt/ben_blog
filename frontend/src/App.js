7f6e1668fd39c78443ff797711f6f9e1fd7fa6fe
DrYogurt <ben.z.rubinstein@gmail.com>
2023-06-15 14:08:41 +0100

    testing using the hash router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Changed BrowserRouter to HashRouter
 import Blog from './components/Blog.js';
 import Header from './components/Header.js';
 import Box from '@mui/material/Box';
